f04be6f698151d1227b775a8e679465c5a695561
Rahul N <rahul.n@ge.com>
2018-04-29 23:25:17 +0530

    webpack + babel

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import divide, { square, cube } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
-
-console.log(square(3));
-console.log(cube(4));
-console.log(divide(4, 2.2));
-
-console.log(isAdult(31));
-console.log(canDrink(21));
-console.log(isSenior(62));
+const template = <p>This is jsx from webpack</p>
+ReactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
