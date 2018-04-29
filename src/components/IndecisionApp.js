fb5ebbf505fe00d1f911c23373a4d10e38124fbf
Rahul N <rahul.n@ge.com>
2018-04-30 02:39:22 +0530

    scss for header, buttons

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -56,26 +56,28 @@ export default class IndecisionApp extends React.Component {
     this.setState((prevState) => ({ options: prevState.options.concat(option) }));
   };
   handleClearSelectedOption = () => {
-    this.setState(() => ({selectedOption: undefined}));
+    this.setState(() => ({ selectedOption: undefined }));
   };
   render() {
     const subTitle = 'A simple todo application';
     return (
       <div>
         <Header subtitle={subTitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal 
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+        <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
